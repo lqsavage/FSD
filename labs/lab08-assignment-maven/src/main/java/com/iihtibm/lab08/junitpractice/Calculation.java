0440fbe9beb5bdf69e37742fdf2e0d12c9371468
Savage <lqsavage@163.com>
2019-10-22 15:35:10 +0800

    Add Junit practise
    Add Junit practise

---
+++ labs/lab08-assignment-maven/src/main/java/com/iihtibm/lab08/junitpractice/Calculation.java
@@ -0,0 +1,11 @@
+package com.iihtibm.lab08.junitpractice;
+
+public class Calculation {
+    public static int add(int a, int b) {
+        return a + b;
+    }
+
+    public static int sub(int a, int b) {
+        return a - b;
+    }
+}
